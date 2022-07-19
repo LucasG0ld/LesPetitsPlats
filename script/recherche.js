6a1c6d9f0460c88ed147e9bf78e5286bf13bf9d9
LucasG0ld <72749416+LucasG0ld@users.noreply.github.com>
2022-07-19 09:49:25 +0200

    Secondary search done

---
+++ script/recherche.js
@@ -0,0 +1,84 @@
+// Recherche ingredients
+function searchInformation() {
+    if (recipesSearchBar.length == 0) {
+        const searchBarIngredient = document.getElementById("lpp-ingredient-searchbar");
+        searchBarIngredient.addEventListener("keyup", e => {
+            const searchString = e.target.value.toLowerCase();
+            const filteredIngredients = triIngredients.filter(ingredient => {
+                return (
+                    ingredient.includes(searchString)
+                );
+            });
+            displayIngredient(filteredIngredients);
+        });
+    }
+}
+
+// Recherche appareils
+function searchInformationAppareil() {
+    const searchBarAppareil = document.getElementById("lpp-appareil-searchbar");
+    searchBarAppareil.addEventListener("keyup", e => {
+        const searchStringAppareil = e.target.value.toLowerCase();
+        const filteredAppareils = triAppareils.filter(appareil => {
+            return (
+                appareil.includes(searchStringAppareil)
+            );
+        });
+        displayAppareil(filteredAppareils);
+    });
+}
+
+// Recherche ustensiles
+function searchInformationUstensile() {
+    const searchBarUstensile = document.getElementById("lpp-ustensile-searchbar");
+    searchBarUstensile.addEventListener("keyup", e => {
+        const searchStringUstensile = e.target.value.toLowerCase();
+        const filteredUstensiles = triUstensiles.filter(ustensile => {
+            return (
+                ustensile.includes(searchStringUstensile)
+            );
+        });
+        displayUstensile(filteredUstensiles);
+    });
+}
+
+//
+function secondarySearchInformation() {
+    const searchBarIngredient = document.getElementById("lpp-ingredient-searchbar");
+        searchBarIngredient.addEventListener("keyup", e => {
+            const searchString = e.target.value.toLowerCase();
+            const filteredIngredients = ingrAfterSearch.filter(ingredient => {
+                return (
+                    ingredient.includes(searchString)
+                );
+            });
+            displayIngredient(filteredIngredients);
+        });
+}
+
+function secondarySearchInformationAppareil() {
+    const searchBarAppareil = document.getElementById("lpp-appareil-searchbar");
+    searchBarAppareil.addEventListener("keyup", e => {
+        const searchStringAppareil = e.target.value.toLowerCase();
+        const filteredAppareils = applianceAfterSearch.filter(appareil => {
+            return (
+                appareil.includes(searchStringAppareil)
+            );
+        });
+        displayAppareil(filteredAppareils);
+    });
+}
+
+// Recherche ustensiles
+function secondarySearchInformationUstensile() {
+    const searchBarUstensile = document.getElementById("lpp-ustensile-searchbar");
+    searchBarUstensile.addEventListener("keyup", e => {
+        const searchStringUstensile = e.target.value.toLowerCase();
+        const filteredUstensiles = ustensilsAfterSearch.filter(ustensile => {
+            return (
+                ustensile.includes(searchStringUstensile)
+            );
+        });
+        displayUstensile(filteredUstensiles);
+    });
+}
